1da6d1a5ee14f97484ad6cb9a0ee42ccf2e9aa58
KwiNam <akn1155@hanmail.net>
2017-07-22 18:47:41 +0900

    7.22 test2

--- app/src/main/java/com/example/kwinam/isafeyou/isafeyou/Fragment/TabFragment1.java
+++ app/src/main/java/com/example/kwinam/isafeyou/isafeyou/Fragment/TabFragment1.java
@@ -17,6 +17,12 @@ public class TabFragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+
+
+
+
+
         return view;
     }
 
