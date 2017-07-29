22abdd8d8f9328bc40cca7e63c473239781ee3bb
KwiNam <akn1155@hanmail.net>
2017-07-30 00:10:04 +0900

    주소록 구현

--- app/src/main/java/com/example/kwinam/isafeyou/isafeyou/Activity/ContactAddActivity.java
+++ app/src/main/java/com/example/kwinam/isafeyou/isafeyou/Activity/ContactAddActivity.java
@@ -1,17 +1,30 @@
 package com.example.kwinam.isafeyou.isafeyou.Activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.kwinam.isafeyou.R;
+import com.google.android.gms.vision.text.Text;
 
 public class ContactAddActivity extends AppCompatActivity {
 
     ImageButton add_decision;
+    ImageButton contact_plus;
+
+    String name = null;
+    String number = null;
+
+    TextView textname;
+    TextView textphone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,10 @@ public class ContactAddActivity extends AppCompatActivity {
         setContentView(R.layout.contact_add);
 
         add_decision = (ImageButton)findViewById(R.id.add_decision_btn);
+        contact_plus = (ImageButton)findViewById(R.id.contact_plus);
+
+        textname = (TextView)findViewById(R.id.contactadd_nameinput);
+        textphone = (TextView)findViewById(R.id.contactadd_phoneinput);
 
         add_decision.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +47,34 @@ public class ContactAddActivity extends AppCompatActivity {
             }
         });
 
+        contact_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK)
+        {
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+            cursor.moveToFirst();
+            name = cursor.getString(0);        //0은 이름을 얻어옵니다.
+            number = cursor.getString(1);   //1은 번호를 받아옵니다.
+            textname.setText(name);
+            textphone.setText(number);
+            cursor.close();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
 }
